6669820158d83924253b5d569fe7c8b6770ceb67
Stanislav Machel <stanislav.machel@gmail.com>
2020-04-25 21:34:49 +0300

    E3 - Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,10 @@ public class PlayerMovement : MonoBehaviour
 {
     // This is a reference to the Rigidbody component called "rb"
     public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +23,16 @@ public class PlayerMovement : MonoBehaviour
     // We marked this as "FixedUpdate" because are using it to mess with physics
     private void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime); // Add a force of 2000 on the z-axis, Time.deltaTime - used that works same for each system mobile or pc with different perfomance
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); // Add a force of 2000 on the z-axis, Time.deltaTime - used that works same for each system mobile or pc with different perfomance
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
