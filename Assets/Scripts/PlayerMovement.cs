4b0e0dfe0b6ffa1a8349f3b34deee3feeed3ffb9
Stanislav Machel <stanislav.machel@gmail.com>
2020-04-25 18:37:02 +0300

    E02 - Programming

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // This is a reference to the Rigidbody component called "rb"
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +14,11 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // We marked this as "FixedUpdate" because are using it to mess with physics
+    private void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime); // Add a force of 2000 on the z-axis, Time.deltaTime - used that works same for each system mobile or pc with different perfomance
     }
 }
